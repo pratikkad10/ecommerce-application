618ac0fdfdb700c3748b430f51e88245dee0c821
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2026-06-25 15:18:44 +0530

    first commit

---
+++ ecommerce-backend/src/app.ts
@@ -0,0 +1,17 @@
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api", routes);
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Server is running" });
+});
+
+// app.use(errorMiddleware);
+
+export default app;
