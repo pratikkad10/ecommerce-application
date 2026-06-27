b5ba0d172dcfca9259ca1fb1fc8a425e80a1c678
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2026-06-27 19:36:48 +0530

    feat: installed cookie-parser and helmet also mounted the user routes to the app.ts

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,17 +1,28 @@
 import express, { type Request, type Response } from "express";
+import userRouter from "./routes/user.routes";
 import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+// Security middleware
+app.use(helmet());
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true
+}));
+
+// Parsing middleware
 app.use(express.json());
+app.use(cookieParser());
 
-// app.use("/api", routes);
-
+// Testing route
 app.get("/", (req: Request, res: Response) => {
     res.json({ message: "Server is running" });
 });
 
-// app.use(errorMiddleware);
+// API routes
+app.use('/api/v1/users', userRouter);
 
 export default app;
