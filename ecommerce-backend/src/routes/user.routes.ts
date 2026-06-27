eedb12ce3fbfe206d582b28b26ab32d6b120b9f4
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2026-06-27 13:06:18 +0530

    feat: implemented the requireauth middleware to user routes

--- ecommerce-backend/src/routes/user.routes.ts
+++ ecommerce-backend/src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getCurrentUserController, loginUserController, logoutUserController, registerUserController } from '../modules/auth/user.auth';
-
+import { requireAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
@@ -8,8 +8,8 @@ router.post('/register', registerUserController);
 
 router.post('/login', loginUserController);
 
-router.get('/me', getCurrentUserController);
+router.get('/me', requireAuth(), getCurrentUserController);
 
-router.get('/logout', logoutUserController);
+router.get('/logout', requireAuth(), logoutUserController);
 
 export default router;
