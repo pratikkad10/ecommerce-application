84117218ff31414986d9242afcb2a45dba28eecb
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2026-07-01 18:03:51 +0530

    updated cors config

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -24,7 +24,7 @@ console.log(process.env.CLIENT_URL);
 // Security middleware
 app.use(helmet());
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [process.env.CLIENT_URL!, "http://localhost:5173"],
     credentials: true
 }));
 
